4438d265c5f7aa8cb900ca5c5401c45e217fcc9d
Trevor Sawler <trevor.sawler@gmail.com>
2016-02-02 09:30:00 -0400

    search

--- resources/views/public/search.blade.php
+++ resources/views/public/search.blade.php
@@ -1,9 +1,33 @@
-@extends('vcms5::public.base-inside')
+@extends('public.base-inside')
 
-@section('bg-class') breadcrumb-1 @stop
-
+@section('browser-title')
+    {!! \Illuminate\Support\Facades\Lang::get('common.catra') !!}
+    {!! $page_title !!}
+@stop
 
 @section('content')
+    <p>&nbsp;</p>
+    <div class="col-md-12">
+        <!-- Form -->
+        {!! Form::open(array(
+        'url' => '/search',
+        'role' => 'form',
+        'name' => 'bookform',
+        'id' => 'bookform',
+        'method' => 'post',
+        'class' => 'form-inline'
+        ))
+        !!}
+        <div class="form-group">
+            <label class="sr-only" for="exampleInputAmount"></label>
+            <div class="input-group">
+                <input type="text" value='{!! $searchterm !!}' class="form-control" name="q" placeholder="">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
+        {!! Form::close() !!}
+        <br/>
+    </div>
     <ul class="list-unstyled">
         @foreach($results as $item)
 
@@ -11,7 +35,7 @@
                 <h4><a href="/{!! $item->slug !!}">{!! $item->page_title !!}</a></h4>
 
                 <p>
-                    {!! str_limit($item->page_content, 200) !!}
+                    {!! str_limit(strip_tags($item->page_content, 200)) !!}
                 </p>
             </li>
 
